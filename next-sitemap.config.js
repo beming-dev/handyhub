543b6091ca1696f61726e37ac4e3ec04fea73ade
beming-dev <mingfordev@gmail.com>
2025-01-26 16:06:02 +0900

    Update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  siteUrl: "https://handyhub-mu.vercel.app",
-  generateRobotsTxt: true,
+  siteUrl: "https://mydomain.com",
+  generateRobotsTxt: true, // robots.txt 생성
+  sitemapSize: 7000,
+  exclude: ["/404"], // 제외할 경로
+  robotsTxtOptions: {
+    policies: [{ userAgent: "*", allow: "/" }],
+  },
 };
