5c4a552df07ccd013cfe0f522545a33183982601
beming-dev <mingfordev@gmail.com>
2025-01-27 19:55:35 +0900

    Update cors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
